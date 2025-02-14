e39e080f869073cd652ec77374bf52d1dee56c54
Repeat-B1nary <repeatbinary0@gmail.com>
2025-02-14 21:21:16 +0000

    Your update message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 import os
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'KingVon'
 app.register_blueprint(IGBP)
 
 
@@ -59,4 +59,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5035)
+    port = int(os.environ.get("PORT", 5035))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port, debug=True)
